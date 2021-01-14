7d51e630444860cd8cac04413edc6afc628fce7d
jhabar <jhabarsinghbhati23@gmail.com>
2021-01-14 22:29:40 +0530

    installed nodemon express body-parser

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res, next) => {
+	res.json({
+		name: "Hello"
+	})
+})
+app.listen(3000);
