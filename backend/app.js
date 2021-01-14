77cdcba6fd3a9d0e9d392b53823dd57a48d44038
jhabar <jhabarsinghbhati23@gmail.com>
2021-01-14 23:04:15 +0530

    added queries and mutaions in graphql

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { URLSearchParams } = require('url');
+const graphqlHttp = require("express-graphql").graphqlHTTP;
+const { buildSchema } = require('graphql');
+
 
 const app = express();
-
+global.URLSearchParams = URLSearchParams;
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-	res.json({
-		name: "Hello"
-	})
-})
+app.use('/graphql', graphqlHttp({
+	schema: buildSchema(`
+		type RootQuery {
+			events: [String!]!
+		}
+
+		type RootMutation {
+			createEvent(name: String): String
+		}
+
+		schema {
+			query: RootQuery 
+			mutation: RootMutation
+		}
+	`),
+	rootValue: {
+		events: () => {
+			return [
+				"Jhabar",
+				"Ram",
+				"Jethu"
+			]
+		},
+		createEvent: (args) => {
+			const eventName = args.name;
+			return eventName;
+		}
+	},
+	graphiql: true,
+}));
+
+
 app.listen(3000);
