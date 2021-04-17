1e02672d64e0528ae2d9a30ada93053614ff3311
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-04-17 07:49:49 +0530

    Added schema in setting

--- getateammate/getateammate/settings.py
+++ getateammate/getateammate/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    "SCHEMA": "users.schema.schema"
+}
+
